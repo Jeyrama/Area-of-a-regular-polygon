078a2b195ffd74fc3c3bad5180113b0f2d51c000
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-11 10:28:11 -0800

    Update Area-of-a-regular-polygon.js

--- Area-of-a-regular-polygon.js
+++ Area-of-a-regular-polygon.js
@@ -10,3 +10,27 @@ If the input string has a wrong format, the function returns "Invalid input".
 
 
 // Solution
+
+function areaOfRegularPolygon(s) {
+  const inputPattern = /^(\d+) sides of (\d+(\.\d+)?) (\w+) each$/;
+
+  const match = s.match(inputPattern);
+
+  if (!match) {
+    return "Invalid input";
+  }
+
+  const numberOfSides = parseInt(match[1]);
+  const sideLength = parseFloat(match[2]);
+  const unit = match[4];
+
+  const pi = Math.PI;
+  const area = (numberOfSides * Math.pow(sideLength, 2)) / (4 * Math.tan(pi / numberOfSides));
+  const roundedArea = area.toFixed(2);
+  
+  if (isNaN(numberOfSides) || isNaN(sideLength) || roundedArea==0) {
+    return "Invalid input";
+  }
+
+  return `${roundedArea} sq.${unit}`;
+}
